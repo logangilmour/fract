fbc3eb1b93b3974dadaf0c2bf054fa2261a0eea1
Logan Gilmour <logangilmour@Logans-MacBook-Air.local>
2012-07-15 22:17:09 -0600

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fract
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/fract/core_test.clj
@@ -0,0 +1,7 @@
+(ns fract.core-test
+  (:use clojure.test
+        fract.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
